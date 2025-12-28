765d90a4c36e8188c9c294aa461f98e013ad6227
DOWRIPILLI PANVISH <119529390+DPanvish@users.noreply.github.com>
2025-12-29 01:38:00 +0530

    DB connection and Tools Setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,14 @@
 import express from "express";
+import { ENV } from "../config/env.js";
+import connectDB from "../config/db.js";
+import { clerkMiddleware } from '@clerk/express'
+
+await connectDB();
 
 const app = express();
-const PORT = 5000;
+const PORT = ENV.PORT;
+
+app.use(clerkMiddleware());
 
 app.get("/", (req, res) => {
     res.send("Welcome to NexBuy")
