2501b4eae0ed6f8ee7bfc59d8505a03a4ec1e2ef
DOWRIPILLI PANVISH <119529390+DPanvish@users.noreply.github.com>
2025-12-31 01:13:10 +0530

    Deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import { ENV } from "../config/env.js";
 import connectDB from "../config/db.js";
 import { clerkMiddleware } from "@clerk/express";
@@ -10,14 +11,22 @@ await connectDB();
 const app = express();
 const PORT = ENV.PORT;
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(clerkMiddleware());
 
 app.use("/api/inngest", serve({client: inngest, functions}));
 
-app.get("/", (req, res) => {
-    res.send("Welcome to NexBuy")
-});
+
+// Making our app ready fro deployment
+if(ENV.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../admin/dist")));
+
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on port on http://localhost:${PORT}`);
