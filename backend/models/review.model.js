b8d86e2d71bb0fed36051f7b81577b5b93c60cfb
DOWRIPILLI PANVISH <119529390+DPanvish@users.noreply.github.com>
2025-12-31 02:16:10 +0530

    Additional fields to Review Model

--- backend/models/review.model.js
+++ backend/models/review.model.js
@@ -21,7 +21,20 @@ const reviewSchema = new mongoose.Schema({
         required: true,
         min: 1,
         max: 5,
+    },
+    comment: {
+        type: String,
+        required: true,
+        minlength: 10,
+        maxlength: 1000,
+    },
+    title: {
+        type: String,
+        maxlength: 100,
     }
 }, {timestamps: true});
+
+// User can review a product only once
+reviewSchema.index({ productId: 1, userId: 1 }, { unique: true });
 
 export const Review = mongoose.model("Review", reviewSchema);
