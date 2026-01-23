a56e1043876e3a7519efd69f8d2fc7fb2ec14ded
DOWRIPILLI PANVISH <119529390+DPanvish@users.noreply.github.com>
2026-01-23 17:38:43 +0530

    Tab layout added

--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -4,7 +4,34 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#ff9300", 
+          light: "#ffa645",        
+          dark: "#cf7200", 
+        },
+        background: {
+          DEFAULT: "#1b1816", 
+          light: "#0b0908",
+          lighter: "#000000",
+        },
+        surface: {
+          DEFAULT: "#2f1b05", 
+          light: "#0b0908",       
+        },
+        text: {
+          primary: "#cdcdcd", 
+          secondary: "#d2ccc7", 
+          tertiary: "#131616",
+        },
+        accent: {
+          DEFAULT: "#43a900",
+          red: "#f35248",     
+          yellow: "#d97708", 
+        },
+      },
+    },
+    plugins: [],
+  }
 }
